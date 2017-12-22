13d8d3e27f3172be69e6cbd1007970bb371bb775
Hyper Mediocrity <michael.treyvaud@gmail.com>
2017-12-22 19:57:56 +0000

    Better error handling

--- src/app.js
+++ src/app.js
@@ -1,43 +1,24 @@
 const express = require('express');
-const path = require('path');
-const logger = require('morgan');
+const Morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
-const users = require('./routes/users');
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(Morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
-const handle404 = function handle404(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+const handle404 = function handle404(req, res) {
+  res.sendStatus(404);
 };
 
 app.use(handle404);
-
-// error handler
-const errorHandler = function errorHandler(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-};
-
-app.use(errorHandler);
 
 module.exports = app;
