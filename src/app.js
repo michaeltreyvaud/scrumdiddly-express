a751df5bc0a01288fed5aeea8d675191f2b13dee
Hyper Mediocrity <michael.treyvaud@gmail.com>
2017-12-22 20:16:44 +0000

    Added auth route

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
+const authRoute = require('./routes/auth');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', index);
+app.use('/auth', authRoute);
 
 // catch 404 and forward to error handler
 const handle404 = function handle404(req, res) {
