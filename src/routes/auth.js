9ae020486dff8b383bdd58b5aa8d4759b8858afe
Hyper Mediocrity <michael.treyvaud@gmail.com>
2017-12-23 03:22:23 +0000

    Added schema validation

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const { Validator } = require('jsonschema');
 
 const router = express.Router();
 
 router.post('/login', (req, res) => {
   const { body } = req;
-  const { email, password } = body;
-  res.status(200).json({
-    email,
-    password,
-  });
+  const jsonValidation = new Validator();
+  const objectSchema = {
+    type: 'object',
+    properties: {
+      email: {
+        type: 'string',
+      },
+      password: {
+        type: 'string',
+      },
+    },
+    required: ['email', 'password'],
+  };
+  if (!jsonValidation.validate(body, objectSchema).valid) {
+    res.sendStatus(400);
+  } else {
+    const { email, password } = body;
+    res.status(200).json({
+      email,
+      password,
+    });
+  }
 });
 
 module.exports = router;
