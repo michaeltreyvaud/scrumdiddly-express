1dfd772eb7923e5a6f181192c6ccd8f5801beab7
Hyper Mediocrity <michael.treyvaud@gmail.com>
2017-12-23 02:38:37 +0000

    removed root

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,10 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.sendStatus(200);
-});
-
 router.post('/login', (req, res) => {
   res.status(200).json({
     hello: 'world',
