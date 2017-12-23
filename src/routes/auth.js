ae01c32defa287c2fcd7f5e48aa8bd0217c931b9
Hyper Mediocrity <michael.treyvaud@gmail.com>
2017-12-23 15:56:38 +0000

    Added signup route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -29,4 +29,30 @@ router.post('/login', (req, res) => {
   }
 });
 
+router.post('/signup', (req, res) => {
+  const { body } = req;
+  const jsonValidation = new Validator();
+  const objectSchema = {
+    type: 'object',
+    properties: {
+      email: {
+        type: 'string',
+      },
+      password: {
+        type: 'string',
+      },
+    },
+    required: ['email', 'password'],
+  };
+  if (!jsonValidation.validate(body, objectSchema).valid) {
+    res.sendStatus(400);
+  } else {
+    const { email, password } = body;
+    res.status(200).json({
+      email,
+      password,
+    });
+  }
+});
+
 module.exports = router;
